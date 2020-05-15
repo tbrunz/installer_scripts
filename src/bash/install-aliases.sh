0905c922f4b2d0c8502de294eede6a806550822d
tbrunz <51008435+tbrunz@users.noreply.github.com>
2020-05-14 19:44:39 -0700

    Update bash aliases & install scriptlet

--- src/bash/install-aliases.sh
+++ src/bash/install-aliases.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
+#
+HOSTS=${1,,}
+HOSTS=${1##-}
 
 echo ">>> cp bash_aliases ~/.bash_aliases "
 cp bash_aliases ~/.bash_aliases
 
-echo ">>> cp bash_hosts ~/.bash_hosts "
-cp bash_hosts ~/.bash_hosts
+if [[ "${HOSTS:0:1}" == "h" ]]; then
+    echo ">>> cp bash_hosts ~/.bash_hosts "
+    cp bash_hosts ~/.bash_hosts
+fi
 
 echo 
 echo "Execute ' . ~/.bash_aliases ' to apply... "
